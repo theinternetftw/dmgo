1fb7b2b20f139d2eede60dcb908da66377f7f68d
theinternetftw <theinternetftw@gmail.com>
2016-12-23 19:59:03 -0500

    Implement MBC2

--- mbc.go
+++ mbc.go
@@ -2,12 +2,26 @@ package dmgo
 
 import "fmt"
 
+// TODO: differentiate between those with
+// batteries (can save) vs those without
+// so we don't leak .sav files for those
+// who don't need em. What about flash
+// mem that doesn't need battery? That
+// used ever?
+// NOTE: bgb warns when carts have RAM
+// but list a cartType that ostensibly
+// doesn't. Real gameboys don't care,
+// do we?
 func makeMBC(cartInfo *CartInfo) mbc {
 	switch cartInfo.CartridgeType {
 	case 0:
 		return &nullMBC{}
 	case 1, 2, 3:
 		return &mbc1{}
+	case 5, 6:
+		return &mbc2{}
+	case 8, 9:
+		return &nullMBC{} // w/ RAM
 	default:
 		panic(fmt.Sprintf("makeMBC: unknown cart type %v", cartInfo.CartridgeType))
 	}
@@ -29,17 +43,26 @@ func (mbc *nullMBC) Read(mem *mem, addr uint16) byte {
 	case addr < 0x8000:
 		return mem.cart[addr]
 	case addr >= 0xa000 && addr < 0xc000:
+		localAddr := uint(addr - 0xa000)
+		if int(localAddr) < len(mem.cartRAM) {
+			return mem.cartRAM[localAddr]
+		}
 		return 0xff
 	default:
 		panic(fmt.Sprintf("nullMBC: not implemented: read at %x\n", addr))
 	}
 }
 func (mbc *nullMBC) Write(mem *mem, addr uint16, val byte) {
-	// if landscape triggers this often, could just access it.
-	if len(mem.cartRAM) != 0 {
-		panic("RAM not implemented in nullMBC!")
+	localAddr := uint(addr - 0xa000)
+	if int(localAddr) < len(mem.cartRAM) {
+		mem.cartRAM[localAddr] = val
 	}
 }
+
+const (
+	bankingModeRAM = iota
+	bankingModeROM
+)
 
 type mbc1 struct {
 	romBankNumber byte
@@ -47,11 +70,6 @@ type mbc1 struct {
 	ramEnabled    bool
 	bankingMode   int
 }
-
-const (
-	bankingModeRAM = iota
-	bankingModeROM
-)
 
 func (mbc *mbc1) Init(mem *mem) {
 	mbc.romBankNumber = 1 // can't go lower
@@ -126,4 +144,69 @@ func (mbc *mbc1) romBankOffset() uint {
 }
 func (mbc *mbc1) ramBankOffset() uint {
 	return uint(mbc.ramBankNumber) * 0x2000
+}
+
+type mbc2 struct {
+	romBankNumber byte
+	ramEnabled    bool
+}
+
+func (mbc *mbc2) Init(mem *mem) {
+	mbc.romBankNumber = 1 // can't go lower
+}
+
+func (mbc *mbc2) Read(mem *mem, addr uint16) byte {
+	switch {
+	case addr < 0x4000:
+		return mem.cart[addr]
+	case addr >= 0x4000 && addr < 0x8000:
+		localAddr := uint(addr-0x4000) + mbc.romBankOffset()
+		return mem.cart[localAddr]
+	case addr >= 0xa000 && addr < 0xc000:
+		localAddr := uint(addr - 0xa000)
+		if mbc.ramEnabled && int(localAddr) < len(mem.cartRAM) {
+			// 4-bit ram (NOTE: pull high nibble down or up?)
+			return mem.cartRAM[localAddr] & 0x0f
+		}
+		return 0xff
+	default:
+		panic(fmt.Sprintf("mbc2: not implemented: read at %x\n", addr))
+	}
+}
+
+func (mbc *mbc2) Write(mem *mem, addr uint16, val byte) {
+	switch {
+	case addr < 0x2000:
+		if addr&0x0100 > 0 {
+			// nop, this bit must be zero
+		} else {
+			mbc.ramEnabled = val&0x0f == 0x0a
+		}
+	case addr >= 0x2000 && addr < 0x4000:
+		if addr&0x0100 == 0 {
+			// nop, this bit must be one
+		} else {
+			// 16 rom banks
+			bankNum := val & 0x0f
+			if bankNum == 0 {
+				// no bank 0 selection.
+				bankNum = 1
+			}
+			mbc.romBankNumber = bankNum
+		}
+	case addr >= 0x4000 && addr < 0x8000:
+		// nop
+	case addr >= 0xa000 && addr < 0xc000:
+		localAddr := uint(addr - 0xa000)
+		if mbc.ramEnabled && int(localAddr) < len(mem.cartRAM) {
+			// 4-bit RAM
+			mem.cartRAM[localAddr] = val & 0x0f
+		}
+	default:
+		panic(fmt.Sprintf("mbc2: not implemented: write at %x\n", addr))
+	}
+}
+
+func (mbc *mbc2) romBankOffset() uint {
+	return uint(mbc.romBankNumber) * 0x4000
 }
