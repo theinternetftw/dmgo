b3c26cace53319c07055ce2563e41075c7a4c612
theinternetftw <theinternetftw@gmail.com>
2016-12-24 00:02:52 -0500

    MBC3 implemented

--- mbc.go
+++ mbc.go
@@ -1,6 +1,9 @@
 package dmgo
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // TODO: differentiate between those with
 // batteries (can save) vs those without
@@ -21,7 +24,11 @@ func makeMBC(cartInfo *CartInfo) mbc {
 	case 5, 6:
 		return &mbc2{}
 	case 8, 9:
-		return &nullMBC{} // w/ RAM
+		return &nullMBC{} // but this time with RAM
+	case 11, 12, 13:
+		panic("MMM01 mapper requested. Not implemented!")
+	case 15, 16, 17, 18, 19:
+		return &mbc3{}
 	default:
 		panic(fmt.Sprintf("makeMBC: unknown cart type %v", cartInfo.CartridgeType))
 	}
@@ -209,4 +216,168 @@ func (mbc *mbc2) Write(mem *mem, addr uint16, val byte) {
 
 func (mbc *mbc2) romBankOffset() uint {
 	return uint(mbc.romBankNumber) * 0x4000
+}
+
+type mbc3 struct {
+	romBankNumber byte
+	ramBankNumber byte
+	ramEnabled    bool
+
+	seconds byte
+	minutes byte
+	hours   byte
+	days    uint16
+
+	latchedSeconds byte
+	latchedMinutes byte
+	latchedHours   byte
+	latchedDays    uint16
+	dayCarry       bool
+
+	timerStopped bool
+	timerLatched bool
+
+	timeAtLastSet time.Time
+}
+
+func (mbc *mbc3) Init(mem *mem) {
+	mbc.romBankNumber = 1 // can't go lower
+
+	// NOTE: could load last time, vals, and carry bit from save or something here, if we want
+	mbc.timeAtLastSet = time.Now()
+}
+
+func (mbc *mbc3) updateTimer() {
+	if mbc.timerStopped {
+		return
+	}
+	oldUnix := time.Unix(
+		int64(mbc.seconds)+
+			int64(mbc.minutes)*60+
+			int64(mbc.hours)*60*60+
+			int64(mbc.days)*60*60*24, 0)
+
+	ticked := time.Now().Sub(mbc.timeAtLastSet)
+	mbc.timeAtLastSet = time.Now()
+
+	newTotalSeconds := oldUnix.Add(ticked).Unix()
+	mbc.seconds = byte(newTotalSeconds % 60)
+
+	newTotalMinutes := (newTotalSeconds - int64(mbc.seconds)/60)
+	mbc.minutes = byte(newTotalMinutes % 60)
+
+	newTotalHours := (newTotalMinutes - int64(mbc.minutes)/60)
+	mbc.hours = byte(newTotalHours % 24)
+
+	newTotalDays := (newTotalHours - int64(mbc.hours)/24)
+	mbc.days = uint16(newTotalDays)
+
+	if newTotalDays > 511 {
+		mbc.dayCarry = true
+	}
+}
+
+func (mbc *mbc3) updateLatch() {
+	mbc.updateTimer()
+	mbc.latchedSeconds = mbc.seconds
+	mbc.latchedMinutes = mbc.minutes
+	mbc.latchedHours = mbc.hours
+	mbc.latchedDays = mbc.days
+}
+
+func (mbc *mbc3) Read(mem *mem, addr uint16) byte {
+	switch {
+	case addr < 0x4000:
+		return mem.cart[addr]
+	case addr >= 0x4000 && addr < 0x8000:
+		localAddr := uint(addr-0x4000) + mbc.romBankOffset()
+		return mem.cart[localAddr]
+	case addr >= 0xa000 && addr < 0xc000:
+		switch mbc.ramBankNumber {
+		case 0, 1, 2, 3:
+			localAddr := uint(addr-0xa000) + mbc.ramBankOffset()
+			if mbc.ramEnabled && int(localAddr) < len(mem.cartRAM) {
+				return mem.cartRAM[localAddr]
+			}
+			return 0xff
+		case 8:
+			return mbc.latchedSeconds
+		case 9:
+			return mbc.latchedMinutes
+		case 10:
+			return mbc.latchedHours
+		case 11:
+			return byte(mbc.latchedDays)
+		case 12:
+			return boolBit(mbc.dayCarry, 7) | boolBit(mbc.timerStopped, 6) | (byte(mbc.latchedDays>>7) & 0x01)
+		}
+		// might need a default of return 0xff here
+	}
+	panic(fmt.Sprintf("mbc3: not implemented: read at %x\n", addr))
+}
+
+func (mbc *mbc3) Write(mem *mem, addr uint16, val byte) {
+	switch {
+	case addr < 0x2000:
+		mbc.ramEnabled = val&0x0f == 0x0a
+	case addr >= 0x2000 && addr < 0x4000:
+		bankNum := val &^ 0x80 // 7bit selector
+		if bankNum == 0 {
+			// no bank 0 selection.
+			bankNum = 1
+		}
+		mbc.romBankNumber = bankNum
+	case addr >= 0x4000 && addr < 0x6000:
+		switch val {
+		case 0, 1, 2, 3:
+			mbc.ramBankNumber = val
+		case 8, 9, 10, 11, 12:
+			mbc.ramBankNumber = val
+		default:
+			// all others nop
+			// NOTE: or should they e.g. select a bank that returns all 0xff's?
+		}
+	case addr >= 0x6000 && addr < 0x8000:
+		if !mbc.timerLatched && val&0x01 > 0 {
+			mbc.timerLatched = true
+			mbc.updateTimer()
+		}
+	case addr >= 0xa000 && addr < 0xc000:
+		switch mbc.ramBankNumber {
+		case 0, 1, 2, 3:
+			localAddr := uint(addr-0xa000) + mbc.ramBankOffset()
+			if mbc.ramEnabled && int(localAddr) < len(mem.cartRAM) {
+				mem.cartRAM[localAddr] = val
+			}
+		case 8:
+			mbc.updateTimer()
+			mbc.seconds = val
+		case 9:
+			mbc.updateTimer()
+			mbc.minutes = val
+		case 10:
+			mbc.updateTimer()
+			mbc.hours = val
+		case 11:
+			mbc.updateTimer()
+			mbc.days = uint16(val)
+		case 12:
+			mbc.updateTimer()
+			mbc.days &^= 0x0100
+			mbc.days |= uint16(val&0x01) << 8
+			mbc.timerStopped = val&(1<<6) > 0
+			mbc.dayCarry = val&(1<<7) > 0
+		default:
+			// nop
+		}
+	default:
+		panic(fmt.Sprintf("mbc3: not implemented: write at %x\n", addr))
+	}
+}
+
+func (mbc *mbc3) romBankOffset() uint {
+	return uint(mbc.romBankNumber) * 0x4000
+}
+func (mbc *mbc3) ramBankOffset() uint {
+	return uint(mbc.ramBankNumber) * 0x2000
 }
