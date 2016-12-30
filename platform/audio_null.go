df522890f9484a859490356e42c80725f4723e47
theinternetftw <theinternetftw@gmail.com>
2016-12-30 03:02:01 -0500

    Add timing to fake audio buffer for apu pacing

--- platform/audio_null.go
+++ platform/audio_null.go
@@ -2,17 +2,29 @@
 
 package platform
 
+import "time"
+
 // AudioBuffer represents a fake audio implementation
 type AudioBuffer struct {
-	BlockCount uint32
-	BlockSize uint32
+	SamplesPerSecond uint32
+	BitsPerSample    uint32
+	ChannelCount     uint32
+	BlockSize        uint32
+	BlockCount       uint32
+
+	fakeBufferConsumed int
+	lastAvailCheck time.Time
 }
 
 // OpenAudioBuffer creates and returns a fake playing buffer.
 func OpenAudioBuffer(blockCount, blockSize, samplesPerSecond, bitsPerSample, channelCount uint32) (*AudioBuffer, error) {
 	ab := AudioBuffer{
+		SamplesPerSecond: samplesPerSecond,
+		BitsPerSample:    bitsPerSample,
+		ChannelCount:     channelCount,
 		BlockCount: blockCount,
 		BlockSize: blockSize,
+		lastAvailCheck: time.Now(),
 	}
 	return &ab, nil
 }
@@ -24,7 +36,14 @@ func (ab *AudioBuffer) Close() error {
 
 // BufferAvailable returns a reasonable value for an available (fake) buffer
 func (ab *AudioBuffer) BufferAvailable() int {
-	return ab.BufferSize() / 2
+	samplesPlayed := int(time.Now().Sub(ab.lastAvailCheck).Seconds() * float64(ab.SamplesPerSecond))
+	bytesWritten := samplesPlayed * int(ab.ChannelCount) * int(ab.BitsPerSample / 8)
+	ab.fakeBufferConsumed -= bytesWritten
+	if ab.fakeBufferConsumed < 0 {
+		ab.fakeBufferConsumed = 0
+	}
+	ab.lastAvailCheck = time.Now()
+	return ab.BufferSize() - ab.fakeBufferConsumed
 }
 
 // BufferSize returns a reasonable size for the fake buffer
@@ -34,5 +53,9 @@ func (ab *AudioBuffer) BufferSize() int {
 
 // Write pretends to write to a fake buffer
 func (ab *AudioBuffer) Write(data []byte) error {
+	ab.fakeBufferConsumed += len(data)
+	if ab.fakeBufferConsumed > ab.BufferSize() {
+		ab.fakeBufferConsumed = ab.BufferSize()
+	}
 	return nil
 }
