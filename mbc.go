3694b09561df97e0d923252bb18991d25a36a854
theinternetftw <theinternetftw@gmail.com>
2017-01-01 01:57:39 -0500

    Snapshots, step 4/N: mbc subtype fields prepared

--- mbc.go
+++ mbc.go
@@ -48,14 +48,14 @@ type mbc interface {
 }
 
 type bankNumbers struct {
-	romBankNumber uint16
-	ramBankNumber uint16
-	maxRAMBank    uint16
-	maxROMBank    uint16
+	ROMBankNumber uint16
+	RAMBankNumber uint16
+	MaxRAMBank    uint16
+	MaxROMBank    uint16
 }
 
 func (bn *bankNumbers) GetROMBankNumber() int {
-	return int(bn.romBankNumber)
+	return int(bn.ROMBankNumber)
 }
 func (bn *bankNumbers) setROMBankNumber(bankNum uint16) {
 	// ran into this in dkland2, which will write trash
@@ -63,29 +63,29 @@ func (bn *bankNumbers) setROMBankNumber(bankNum uint16) {
 	// don't have the lines hooked up, so let's only
 	// "hook up" lines that are actually addressable
 	topBit := uint16(0x8000)
-	for bn.maxROMBank < topBit {
+	for bn.MaxROMBank < topBit {
 		bankNum &^= topBit
 		topBit >>= 1
 	}
-	bn.romBankNumber = bankNum
+	bn.ROMBankNumber = bankNum
 }
 func (bn *bankNumbers) setRAMBankNumber(bankNum uint16) {
 	topBit := uint16(0x8000)
-	for bn.maxRAMBank < topBit {
+	for bn.MaxRAMBank < topBit {
 		bankNum &^= topBit
 		topBit >>= 1
 	}
-	bn.ramBankNumber = bankNum
+	bn.RAMBankNumber = bankNum
 }
 func (bn *bankNumbers) init(mem *mem) {
-	bn.maxROMBank = uint16(len(mem.cart)/0x4000 - 1)
-	bn.maxRAMBank = uint16(len(mem.CartRAM)/0x2000 - 1)
+	bn.MaxROMBank = uint16(len(mem.cart)/0x4000 - 1)
+	bn.MaxRAMBank = uint16(len(mem.CartRAM)/0x2000 - 1)
 }
 func (bn *bankNumbers) romBankOffset() uint {
-	return uint(bn.romBankNumber) * 0x4000
+	return uint(bn.ROMBankNumber) * 0x4000
 }
 func (bn *bankNumbers) ramBankOffset() uint {
-	return uint(bn.ramBankNumber) * 0x2000
+	return uint(bn.RAMBankNumber) * 0x2000
 }
 
 type nullMBC struct {
@@ -94,7 +94,7 @@ type nullMBC struct {
 
 func (mbc *nullMBC) Init(mem *mem) {
 	mbc.bankNumbers.init(mem)
-	mbc.romBankNumber = 1 // set up a flat map
+	mbc.ROMBankNumber = 1 // set up a flat map
 }
 func (mbc *nullMBC) Read(mem *mem, addr uint16) byte {
 	switch {
@@ -125,13 +125,13 @@ const (
 type mbc1 struct {
 	bankNumbers
 
-	ramEnabled  bool
-	bankingMode int
+	RAMEnabled  bool
+	BankingMode int
 }
 
 func (mbc *mbc1) Init(mem *mem) {
 	mbc.bankNumbers.init(mem)
-	mbc.romBankNumber = 1 // can't go lower
+	mbc.ROMBankNumber = 1 // can't go lower
 }
 
 func (mbc *mbc1) Read(mem *mem, addr uint16) byte {
@@ -141,12 +141,12 @@ func (mbc *mbc1) Read(mem *mem, addr uint16) byte {
 	case addr >= 0x4000 && addr < 0x8000:
 		localAddr := uint(addr-0x4000) + mbc.romBankOffset()
 		if localAddr >= uint(len(mem.cart)) {
-			panic(fmt.Sprintf("mbc1: bad rom local addr: 0x%06x, bank number: %d\r\n", localAddr, mbc.romBankNumber))
+			panic(fmt.Sprintf("mbc1: bad rom local addr: 0x%06x, bank number: %d\r\n", localAddr, mbc.ROMBankNumber))
 		}
 		return mem.cart[localAddr]
 	case addr >= 0xa000 && addr < 0xc000:
 		localAddr := uint(addr-0xa000) + mbc.ramBankOffset()
-		if mbc.ramEnabled && int(localAddr) < len(mem.CartRAM) {
+		if mbc.RAMEnabled && int(localAddr) < len(mem.CartRAM) {
 			return mem.CartRAM[localAddr]
 		}
 		return 0xff
@@ -158,7 +158,7 @@ func (mbc *mbc1) Read(mem *mem, addr uint16) byte {
 func (mbc *mbc1) Write(mem *mem, addr uint16, val byte) {
 	switch {
 	case addr < 0x2000:
-		mbc.ramEnabled = val&0x0f == 0x0a
+		mbc.RAMEnabled = val&0x0f == 0x0a
 	case addr >= 0x2000 && addr < 0x4000:
 		bankNum := uint16(val & 0x1f)
 		if bankNum == 0 {
@@ -169,30 +169,30 @@ func (mbc *mbc1) Write(mem *mem, addr uint16, val byte) {
 			// 128-3, for MBC1
 			bankNum = 1
 		}
-		bankNum = (mbc.romBankNumber &^ 0x1f) | bankNum
+		bankNum = (mbc.ROMBankNumber &^ 0x1f) | bankNum
 		mbc.setROMBankNumber(bankNum)
 	case addr >= 0x4000 && addr < 0x6000:
 		valBits := uint16(val & 0x03)
-		if mbc.bankingMode == bankingModeRAM {
+		if mbc.BankingMode == bankingModeRAM {
 			mbc.setRAMBankNumber(valBits)
 		} else { // ROM mode
-			bankNum := (valBits << 5) | (mbc.romBankNumber & 0x1f)
+			bankNum := (valBits << 5) | (mbc.ROMBankNumber & 0x1f)
 			mbc.setROMBankNumber(bankNum)
 		}
 	case addr >= 0x6000 && addr < 0x8000:
 		// NOTE: do those two bits from the RAM number need to
 		// be passed over to the ROM number after the banking
 		// mode changes? (and vice versa?)
-		if (val&0x01) > 0 && mbc.bankingMode != bankingModeRAM {
-			mbc.bankingMode = bankingModeRAM
-			mbc.setROMBankNumber(mbc.romBankNumber & 0x1f)
+		if (val&0x01) > 0 && mbc.BankingMode != bankingModeRAM {
+			mbc.BankingMode = bankingModeRAM
+			mbc.setROMBankNumber(mbc.ROMBankNumber & 0x1f)
 		} else {
-			mbc.bankingMode = bankingModeROM
+			mbc.BankingMode = bankingModeROM
 			mbc.setRAMBankNumber(0)
 		}
 	case addr >= 0xa000 && addr < 0xc000:
 		localAddr := uint(addr-0xa000) + mbc.ramBankOffset()
-		if mbc.ramEnabled && int(localAddr) < len(mem.CartRAM) {
+		if mbc.RAMEnabled && int(localAddr) < len(mem.CartRAM) {
 			mem.CartRAM[localAddr] = val
 		}
 	default:
@@ -203,12 +203,12 @@ func (mbc *mbc1) Write(mem *mem, addr uint16, val byte) {
 type mbc2 struct {
 	bankNumbers
 
-	ramEnabled bool
+	RAMEnabled bool
 }
 
 func (mbc *mbc2) Init(mem *mem) {
 	mbc.bankNumbers.init(mem)
-	mbc.romBankNumber = 1 // can't go lower
+	mbc.ROMBankNumber = 1 // can't go lower
 }
 
 func (mbc *mbc2) Read(mem *mem, addr uint16) byte {
@@ -218,12 +218,12 @@ func (mbc *mbc2) Read(mem *mem, addr uint16) byte {
 	case addr >= 0x4000 && addr < 0x8000:
 		localAddr := uint(addr-0x4000) + mbc.romBankOffset()
 		if localAddr >= uint(len(mem.cart)) {
-			panic(fmt.Sprintf("mbc2: bad rom local addr: 0x%06x, bank number: %d\r\n", localAddr, mbc.romBankNumber))
+			panic(fmt.Sprintf("mbc2: bad rom local addr: 0x%06x, bank number: %d\r\n", localAddr, mbc.ROMBankNumber))
 		}
 		return mem.cart[localAddr]
 	case addr >= 0xa000 && addr < 0xc000:
 		localAddr := uint(addr - 0xa000)
-		if mbc.ramEnabled && int(localAddr) < len(mem.CartRAM) {
+		if mbc.RAMEnabled && int(localAddr) < len(mem.CartRAM) {
 			// 4-bit ram (FIXME: pull high nibble down or up?)
 			return mem.CartRAM[localAddr] & 0x0f
 		}
@@ -239,7 +239,7 @@ func (mbc *mbc2) Write(mem *mem, addr uint16, val byte) {
 		if addr&0x0100 > 0 {
 			// nop, this bit must be zero
 		} else {
-			mbc.ramEnabled = val&0x0f == 0x0a
+			mbc.RAMEnabled = val&0x0f == 0x0a
 		}
 	case addr >= 0x2000 && addr < 0x4000:
 		if addr&0x0100 == 0 {
@@ -257,7 +257,7 @@ func (mbc *mbc2) Write(mem *mem, addr uint16, val byte) {
 		// nop
 	case addr >= 0xa000 && addr < 0xc000:
 		localAddr := uint(addr - 0xa000)
-		if mbc.ramEnabled && int(localAddr) < len(mem.CartRAM) {
+		if mbc.RAMEnabled && int(localAddr) < len(mem.CartRAM) {
 			// 4-bit RAM
 			mem.CartRAM[localAddr] = val & 0x0f
 		}
@@ -269,71 +269,71 @@ func (mbc *mbc2) Write(mem *mem, addr uint16, val byte) {
 type mbc3 struct {
 	bankNumbers
 
-	ramEnabled bool
+	RAMEnabled bool
 
-	seconds byte
-	minutes byte
-	hours   byte
-	days    uint16
+	Seconds byte
+	Minutes byte
+	Hours   byte
+	Days    uint16
 
-	latchedSeconds byte
-	latchedMinutes byte
-	latchedHours   byte
-	latchedDays    uint16
-	dayCarry       bool
+	LatchedSeconds byte
+	LatchedMinutes byte
+	LatchedHours   byte
+	LatchedDays    uint16
+	DayCarry       bool
 
-	timerStopped bool
-	timerLatched bool
+	TimerStopped bool
+	TimerLatched bool
 
-	timeAtLastSet time.Time
+	TimeAtLastSet time.Time
 }
 
 func (mbc *mbc3) Init(mem *mem) {
 	mbc.bankNumbers.init(mem)
-	mbc.romBankNumber = 1 // can't go lower
+	mbc.ROMBankNumber = 1 // can't go lower
 
 	// NOTE: could load this/vals/carry from save or something
 	// here. Considering .sav is a simple format (just the RAM
 	// proper), should make e.g. an additional .rtc file for this
-	mbc.timeAtLastSet = time.Now()
+	mbc.TimeAtLastSet = time.Now()
 }
 
 func (mbc *mbc3) updateTimer() {
-	if mbc.timerStopped {
+	if mbc.TimerStopped {
 		return
 	}
 	oldUnix := time.Unix(
-		int64(mbc.seconds)+
-			int64(mbc.minutes)*60+
-			int64(mbc.hours)*60*60+
-			int64(mbc.days)*60*60*24, 0)
+		int64(mbc.Seconds)+
+			int64(mbc.Minutes)*60+
+			int64(mbc.Hours)*60*60+
+			int64(mbc.Days)*60*60*24, 0)
 
-	ticked := time.Now().Sub(mbc.timeAtLastSet)
-	mbc.timeAtLastSet = time.Now()
+	ticked := time.Now().Sub(mbc.TimeAtLastSet)
+	mbc.TimeAtLastSet = time.Now()
 
 	newTotalSeconds := oldUnix.Add(ticked).Unix()
-	mbc.seconds = byte(newTotalSeconds % 60)
+	mbc.Seconds = byte(newTotalSeconds % 60)
 
-	newTotalMinutes := (newTotalSeconds - int64(mbc.seconds)/60)
-	mbc.minutes = byte(newTotalMinutes % 60)
+	newTotalMinutes := (newTotalSeconds - int64(mbc.Seconds)/60)
+	mbc.Minutes = byte(newTotalMinutes % 60)
 
-	newTotalHours := (newTotalMinutes - int64(mbc.minutes)/60)
-	mbc.hours = byte(newTotalHours % 24)
+	newTotalHours := (newTotalMinutes - int64(mbc.Minutes)/60)
+	mbc.Hours = byte(newTotalHours % 24)
 
-	newTotalDays := (newTotalHours - int64(mbc.hours)/24)
-	mbc.days = uint16(newTotalDays)
+	newTotalDays := (newTotalHours - int64(mbc.Hours)/24)
+	mbc.Days = uint16(newTotalDays)
 
 	if newTotalDays > 511 {
-		mbc.dayCarry = true
+		mbc.DayCarry = true
 	}
 }
 
 func (mbc *mbc3) updateLatch() {
 	mbc.updateTimer()
-	mbc.latchedSeconds = mbc.seconds
-	mbc.latchedMinutes = mbc.minutes
-	mbc.latchedHours = mbc.hours
-	mbc.latchedDays = mbc.days
+	mbc.LatchedSeconds = mbc.Seconds
+	mbc.LatchedMinutes = mbc.Minutes
+	mbc.LatchedHours = mbc.Hours
+	mbc.LatchedDays = mbc.Days
 }
 
 func (mbc *mbc3) Read(mem *mem, addr uint16) byte {
@@ -343,27 +343,27 @@ func (mbc *mbc3) Read(mem *mem, addr uint16) byte {
 	case addr >= 0x4000 && addr < 0x8000:
 		localAddr := uint(addr-0x4000) + mbc.romBankOffset()
 		if localAddr >= uint(len(mem.cart)) {
-			panic(fmt.Sprintf("mbc3: bad rom local addr: 0x%06x, bank number: %d\r\n", localAddr, mbc.romBankNumber))
+			panic(fmt.Sprintf("mbc3: bad rom local addr: 0x%06x, bank number: %d\r\n", localAddr, mbc.ROMBankNumber))
 		}
 		return mem.cart[localAddr]
 	case addr >= 0xa000 && addr < 0xc000:
-		switch mbc.ramBankNumber {
+		switch mbc.RAMBankNumber {
 		case 0, 1, 2, 3:
 			localAddr := uint(addr-0xa000) + mbc.ramBankOffset()
-			if mbc.ramEnabled && int(localAddr) < len(mem.CartRAM) {
+			if mbc.RAMEnabled && int(localAddr) < len(mem.CartRAM) {
 				return mem.CartRAM[localAddr]
 			}
 			return 0xff
 		case 8:
-			return mbc.latchedSeconds
+			return mbc.LatchedSeconds
 		case 9:
-			return mbc.latchedMinutes
+			return mbc.LatchedMinutes
 		case 10:
-			return mbc.latchedHours
+			return mbc.LatchedHours
 		case 11:
-			return byte(mbc.latchedDays)
+			return byte(mbc.LatchedDays)
 		case 12:
-			return boolBit(mbc.dayCarry, 7) | boolBit(mbc.timerStopped, 6) | (byte(mbc.latchedDays>>7) & 0x01)
+			return boolBit(mbc.DayCarry, 7) | boolBit(mbc.TimerStopped, 6) | (byte(mbc.LatchedDays>>7) & 0x01)
 		}
 		// might need a default of return 0xff here
 	}
@@ -373,7 +373,7 @@ func (mbc *mbc3) Read(mem *mem, addr uint16) byte {
 func (mbc *mbc3) Write(mem *mem, addr uint16, val byte) {
 	switch {
 	case addr < 0x2000:
-		mbc.ramEnabled = val&0x0f == 0x0a
+		mbc.RAMEnabled = val&0x0f == 0x0a
 	case addr >= 0x2000 && addr < 0x4000:
 		bankNum := uint16(val &^ 0x80) // 7bit selector
 		if bankNum == 0 {
@@ -387,41 +387,44 @@ func (mbc *mbc3) Write(mem *mem, addr uint16, val byte) {
 			mbc.setRAMBankNumber(uint16(val))
 		case 8, 9, 10, 11, 12:
 			// sidestep the bank set semantics for the rtc regs
-			mbc.ramBankNumber = uint16(val)
+			mbc.RAMBankNumber = uint16(val)
 		default:
 			// all others nop
 			// NOTE: or should they e.g. select a bank that returns all 0xff's?
 		}
 	case addr >= 0x6000 && addr < 0x8000:
-		if !mbc.timerLatched && val&0x01 > 0 {
-			mbc.timerLatched = true
+		switch {
+		case val&0x01 == 0:
+			mbc.TimerLatched = false
+		case val&0x01 == 1 && !mbc.TimerLatched:
+			mbc.TimerLatched = true
 			mbc.updateTimer()
 		}
 	case addr >= 0xa000 && addr < 0xc000:
-		switch mbc.ramBankNumber {
+		switch mbc.RAMBankNumber {
 		case 0, 1, 2, 3:
 			localAddr := uint(addr-0xa000) + mbc.ramBankOffset()
-			if mbc.ramEnabled && int(localAddr) < len(mem.CartRAM) {
+			if mbc.RAMEnabled && int(localAddr) < len(mem.CartRAM) {
 				mem.CartRAM[localAddr] = val
 			}
 		case 8:
 			mbc.updateTimer()
-			mbc.seconds = val
+			mbc.Seconds = val
 		case 9:
 			mbc.updateTimer()
-			mbc.minutes = val
+			mbc.Minutes = val
 		case 10:
 			mbc.updateTimer()
-			mbc.hours = val
+			mbc.Hours = val
 		case 11:
 			mbc.updateTimer()
-			mbc.days = uint16(val)
+			mbc.Days = uint16(val)
 		case 12:
 			mbc.updateTimer()
-			mbc.days &^= 0x0100
-			mbc.days |= uint16(val&0x01) << 8
-			mbc.timerStopped = val&(1<<6) > 0
-			mbc.dayCarry = val&(1<<7) > 0
+			mbc.Days &^= 0x0100
+			mbc.Days |= uint16(val&0x01) << 8
+			mbc.TimerStopped = val&(1<<6) > 0
+			mbc.DayCarry = val&(1<<7) > 0
 		default:
 			// nop
 		}
@@ -433,14 +436,14 @@ func (mbc *mbc3) Write(mem *mem, addr uint16, val byte) {
 type mbc5 struct {
 	bankNumbers
 
-	ramEnabled bool
+	RAMEnabled bool
 }
 
 func (mbc *mbc5) Init(mem *mem) {
 	mbc.bankNumbers.init(mem)
 
 	// NOTE: can do bank 0 now, but still start with 1
-	mbc.romBankNumber = 1
+	mbc.ROMBankNumber = 1
 }
 
 func (mbc *mbc5) Read(mem *mem, addr uint16) byte {
@@ -450,12 +453,12 @@ func (mbc *mbc5) Read(mem *mem, addr uint16) byte {
 	case addr >= 0x4000 && addr < 0x8000:
 		localAddr := uint(addr-0x4000) + mbc.romBankOffset()
 		if localAddr >= uint(len(mem.cart)) {
-			panic(fmt.Sprintf("mbc5: bad rom local addr: 0x%06x, bank number: %d\r\n", localAddr, mbc.romBankNumber))
+			panic(fmt.Sprintf("mbc5: bad rom local addr: 0x%06x, bank number: %d\r\n", localAddr, mbc.ROMBankNumber))
 		}
 		return mem.cart[localAddr]
 	case addr >= 0xa000 && addr < 0xc000:
 		localAddr := uint(addr-0xa000) + mbc.ramBankOffset()
-		if mbc.ramEnabled && int(localAddr) < len(mem.CartRAM) {
+		if mbc.RAMEnabled && int(localAddr) < len(mem.CartRAM) {
 			return mem.CartRAM[localAddr]
 		}
 		return 0xff
@@ -467,21 +470,21 @@ func (mbc *mbc5) Read(mem *mem, addr uint16) byte {
 func (mbc *mbc5) Write(mem *mem, addr uint16, val byte) {
 	switch {
 	case addr < 0x2000:
-		mbc.ramEnabled = val&0x0f == 0x0a
+		mbc.RAMEnabled = val&0x0f == 0x0a
 	case addr >= 0x2000 && addr < 0x3000:
-		mbc.setROMBankNumber((mbc.romBankNumber &^ 0xff) | uint16(val))
+		mbc.setROMBankNumber((mbc.ROMBankNumber &^ 0xff) | uint16(val))
 	case addr >= 0x3000 && addr < 0x4000:
 		// NOTE: TCAGBD says that games that don't use the 9th bit
 		// can use this to set the lower eight! I'll wait until I
 		// see a game try to do that before impl'ing
-		mbc.setROMBankNumber((mbc.romBankNumber &^ 0x100) | uint16(val&0x01)<<8)
+		mbc.setROMBankNumber((mbc.ROMBankNumber &^ 0x100) | uint16(val&0x01)<<8)
 	case addr >= 0x4000 && addr < 0x6000:
 		mbc.setRAMBankNumber(uint16(val & 0x0f))
 	case addr >= 0x6000 && addr < 0x8000:
 		// nop?
 	case addr >= 0xa000 && addr < 0xc000:
 		localAddr := uint(addr-0xa000) + mbc.ramBankOffset()
-		if mbc.ramEnabled && int(localAddr) < len(mem.CartRAM) {
+		if mbc.RAMEnabled && int(localAddr) < len(mem.CartRAM) {
 			mem.CartRAM[localAddr] = val
 		}
 	default:
