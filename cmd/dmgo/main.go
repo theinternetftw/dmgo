7662113419fd7cc8292f035dc1499a2c17ccb89e
theinternetftw <theinternetftw@gmail.com>
2018-09-05 22:47:34 -0400

    Poll input at 125Hz and measure rtime and ftime

--- cmd/dmgo/main.go
+++ cmd/dmgo/main.go
@@ -63,8 +63,12 @@ func startHeadlessEmu(emu dmgo.Emulator) {
 func startEmu(filename string, window *platform.WindowState, emu dmgo.Emulator) {
 
 	// FIXME: settings are for debug right now
-	lastVBlankTime := time.Now()
+	lastFlipTime := time.Now()
 	lastSaveTime := time.Now()
+	lastInputPollTime := time.Now()
+
+	timer := time.NewTimer(0)
+	<-timer.C
 
 	snapshotPrefix := filename + ".snapshot"
 
@@ -81,52 +85,68 @@ func startEmu(filename string, window *platform.WindowState, emu dmgo.Emulator) 
 	workingAudioBuffer := make([]byte, audio.BufferSize())
 	dieIf(err)
 
+	maxRDiff := time.Duration(0)
+	maxFDiff := time.Duration(0)
+	frameCount := 0
+
+	frametimeGoal := 1.0/60.0
+
 	snapshotMode := 'x'
 
-	for {
-		window.Mutex.Lock()
-		newInput := dmgo.Input {
-			Joypad: dmgo.Joypad {
-				Sel:  window.CharIsDown('t'), Start: window.CharIsDown('y'),
-				Up:   window.CharIsDown('w'), Down:  window.CharIsDown('s'),
-				Left: window.CharIsDown('a'), Right: window.CharIsDown('d'),
-				A:    window.CharIsDown('k'), B:     window.CharIsDown('j'),
-			},
-		}
-		numDown := 'x'
-		for r := '0'; r <= '9'; r++ {
-			if window.CharIsDown(r) {
-				numDown = r
-				break
-			}
-		}
-		if window.CharIsDown('m') {
-			snapshotMode = 'm'
-		} else if window.CharIsDown('l') {
-			snapshotMode = 'l'
-		}
-		window.Mutex.Unlock()
+	newInput := dmgo.Input{}
 
-		if numDown > '0' && numDown <= '9' {
-			snapFilename := snapshotPrefix+string(numDown)
-			if snapshotMode == 'm' {
-				snapshotMode = 'x'
-				snapshot := emu.MakeSnapshot()
-				ioutil.WriteFile(snapFilename, snapshot, os.FileMode(0644))
-			} else if snapshotMode == 'l' {
-				snapshotMode = 'x'
-				snapBytes, err := ioutil.ReadFile(snapFilename)
-				if err != nil {
-					fmt.Println("failed to load snapshot:", err)
-					continue
-				}
-				newEmu, err := emu.LoadSnapshot(snapBytes)
-				if err != nil {
-					fmt.Println("failed to load snapshot:", err)
-					continue
-				}
-				emu = newEmu
+	for {
+
+		now := time.Now()
+
+		inputDiff := now.Sub(lastInputPollTime)
+
+		if inputDiff > 8*time.Millisecond {
+			window.Mutex.Lock()
+			newInput = dmgo.Input {
+				Joypad: dmgo.Joypad {
+					Sel:  window.CharIsDown('t'), Start: window.CharIsDown('y'),
+					Up:   window.CharIsDown('w'), Down:  window.CharIsDown('s'),
+					Left: window.CharIsDown('a'), Right: window.CharIsDown('d'),
+					A:    window.CharIsDown('k'), B:     window.CharIsDown('j'),
+				},
 			}
+			numDown := 'x'
+			for r := '0'; r <= '9'; r++ {
+				if window.CharIsDown(r) {
+					numDown = r
+					break
+				}
+			}
+			if window.CharIsDown('m') {
+				snapshotMode = 'm'
+			} else if window.CharIsDown('l') {
+				snapshotMode = 'l'
+			}
+			window.Mutex.Unlock()
+
+			if numDown > '0' && numDown <= '9' {
+				snapFilename := snapshotPrefix+string(numDown)
+				if snapshotMode == 'm' {
+					snapshotMode = 'x'
+					snapshot := emu.MakeSnapshot()
+					ioutil.WriteFile(snapFilename, snapshot, os.FileMode(0644))
+				} else if snapshotMode == 'l' {
+					snapshotMode = 'x'
+					snapBytes, err := ioutil.ReadFile(snapFilename)
+					if err != nil {
+						fmt.Println("failed to load snapshot:", err)
+						continue
+					}
+					newEmu, err := emu.LoadSnapshot(snapBytes)
+					if err != nil {
+						fmt.Println("failed to load snapshot:", err)
+						continue
+					}
+					emu = newEmu
+				}
+			}
+			lastInputPollTime = time.Now()
 		}
 
 		emu.UpdateInput(newInput)
@@ -148,12 +168,39 @@ func startEmu(filename string, window *platform.WindowState, emu dmgo.Emulator) 
 			window.RequestDraw()
 			window.Mutex.Unlock()
 
-			spent := time.Now().Sub(lastVBlankTime)
-			toWait := 17*time.Millisecond - spent
+			rDiff := time.Now().Sub(lastFlipTime)
+
+			// hack to get better accuracy, could do
+			// a two stage wait with spin at the end,
+			// but that really adds to the cycles
+			fudge := 2000000*time.Nanosecond
+
+			toWait := 16600000*time.Nanosecond - rDiff - fudge
 			if toWait > time.Duration(0) {
 				<-time.NewTimer(toWait).C
 			}
-			lastVBlankTime = time.Now()
+
+			frameStart := lastFlipTime
+			lastFlipTime = time.Now()
+
+			fDiff := time.Now().Sub(frameStart)
+			if rDiff > maxRDiff {
+				maxRDiff = rDiff
+			}
+			if fDiff > maxFDiff {
+				maxFDiff = fDiff
+			}
+
+			frameCount++
+			if frameCount & 0xff == 0 {
+				fmt.Printf("maxRTime %.4f, maxFTime %.4f\n", maxRDiff.Seconds(), maxFDiff.Seconds())
+				maxRDiff = 0
+				maxFDiff = 0
+				if frametimeGoal == 0 {
+					frametimeGoal = 1
+				}
+			}
+
 		}
 		if time.Now().Sub(lastSaveTime) > 5*time.Second {
 			ram := emu.GetCartRAM()
