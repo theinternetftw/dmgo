ef40d7ae0a0d65f3556dbb154b990b22a2669a27
theinternetftw <theinternetftw@gmail.com>
2024-01-02 03:10:53 -0500

    Add break on field val set/change

--- dbg.go
+++ dbg.go
@@ -12,11 +12,42 @@ const (
 	dbgStateRun
 )
 
+const (
+	breakOpChange int = iota
+	breakOpEq
+	breakOpNeq
+
+	// TODO: add these once we parse vals/args into ints and such
+	// breakOpGt
+	// breakOpGte
+	// breakOpLt
+	// breakOpLte
+)
+
+var breakOpsMap = map[string]int{
+	"change": breakOpChange,
+	"=":      breakOpEq,
+	"==":     breakOpEq,
+	"!=":     breakOpNeq,
+
+	// ">": breakOpGt,
+	// ">=": breakOpGte,
+	// "<": breakOpLt,
+	// "<=": breakOpLte,
+}
+
+type breakpoint struct {
+	fieldPath string
+	breakVal  string
+	op        int
+}
+
 type debugger struct {
 	keysJustPressed [256]bool
 	keys            [256]bool
 	lineBuf         []byte
 	state           int
+	breakpoints     []breakpoint
 }
 
 func lookupValue(root reflect.Value, lookups []string) (reflect.Value, bool) {
@@ -36,10 +67,12 @@ func lookupValue(root reflect.Value, lookups []string) (reflect.Value, bool) {
 	}
 	return v, true
 }
-func getField(root reflect.Value, path string) (reflect.Value, bool) {
+func getField(emu Emulator, path string) (reflect.Value, bool) {
+	root := reflect.Indirect(reflect.ValueOf(emu))
 	return lookupValue(root, strings.Split(path, "."))
 }
-func getMethod(root reflect.Value, path string) (reflect.Value, bool) {
+func getMethod(emu Emulator, path string) (reflect.Value, bool) {
+	root := reflect.Indirect(reflect.ValueOf(emu))
 	v := root
 	parts := strings.Split(path, ".")
 	if len(parts) > 1 {
@@ -50,8 +83,6 @@ func getMethod(root reflect.Value, path string) (reflect.Value, bool) {
 		}
 	}
 	t := v.Type()
-	fmt.Println(t)
-	fmt.Println(parts[len(parts)-1])
 	var ok bool
 	_, ok = t.MethodByName(parts[len(parts)-1])
 	if ok {
@@ -60,13 +91,21 @@ func getMethod(root reflect.Value, path string) (reflect.Value, bool) {
 	// also allow pointer receivers
 	v = v.Addr()
 	t = v.Type()
-	fmt.Println(t)
 	_, ok = t.MethodByName(parts[len(parts)-1])
 	if ok {
 		return v.MethodByName(parts[len(parts)-1]), true
 	}
 	fmt.Println("method not found or private")
 	return reflect.Value{}, false
+}
+
+func strIndexOf(strs []string, str string) int {
+	for i := range strs {
+		if strs[i] == str {
+			return i
+		}
+	}
+	return -1
 }
 
 var dbgCmdMap = map[string]func(*debugger, Emulator, []string){
@@ -76,21 +115,53 @@ var dbgCmdMap = map[string]func(*debugger, Emulator, []string){
 	"x": func(d *debugger, emu Emulator, arg []string) {
 		if len(arg) == 0 {
 			fmt.Println("usage: x FIELD_PATH")
+			return
 		}
-		root := reflect.Indirect(reflect.ValueOf(emu))
-		if v, ok := getField(root, arg[0]); ok {
+		if v, ok := getField(emu, arg[0]); ok {
 			fmt.Println(v)
 		}
+	},
+	"break": func(d *debugger, emu Emulator, arg []string) {
+		if len(arg) == 0 {
+			fmt.Println("usage: break FIELD_NAME OP [VAL]")
+			return
+		}
+		v, field_ok := getField(emu, arg[0])
+		if !field_ok {
+			return
+		}
+		opStr := "change"
+		if len(arg) > 1 {
+			opStr = arg[1]
+		}
+		op, op_ok := breakOpsMap[opStr]
+		if !op_ok {
+			fmt.Println("bad OP arg for break")
+			return
+		}
+		var valStr string
+		if op != breakOpChange {
+			if len(arg) < 3 {
+				fmt.Println("need val for break op of", opStr)
+				return
+			}
+			valStr = arg[2]
+		} else {
+			valStr = fmt.Sprintf("%v", v) // change works like != lastVal
+		}
+		bp := breakpoint{fieldPath: arg[0], op: op, breakVal: valStr}
+		d.breakpoints = append(d.breakpoints, bp)
 	},
 	"call": func(d *debugger, emu Emulator, arg []string) {
 		if len(arg) == 0 {
 			fmt.Println("usage: call METHOD_PATH")
+			return
 		}
 		if len(arg) > 1 {
 			fmt.Println("method args not yet impl")
+			return
 		}
-		root := reflect.Indirect(reflect.ValueOf(emu))
-		if v, ok := getMethod(root, arg[0]); ok {
+		if v, ok := getMethod(emu, arg[0]); ok {
 			results := v.Call([]reflect.Value{})
 			if len(results) > 0 {
 				fmt.Println(results)
@@ -133,6 +204,42 @@ func (d *debugger) step(emu Emulator) {
 			}
 		}
 	case dbgStateRun:
+		for i := range d.breakpoints {
+			bp := &d.breakpoints[i]
+			f, ok := getField(emu, bp.fieldPath)
+			if !ok {
+				fmt.Println("couldn't find field listed in breakpoint, something screwy's going on...")
+				d.state = dbgStateNewCmd
+				return
+			}
+			valStr := fmt.Sprintf("%v", f)
+			// fmt.Println("checking bp for", bp.fieldPath, "val is", valStr)
+			switch bp.op {
+			case breakOpChange:
+				if valStr != bp.breakVal {
+					fmt.Println("hit breakpoint:", bp.fieldPath, "changed from", bp.breakVal, "to", valStr)
+					bp.breakVal = valStr
+					d.state = dbgStateNewCmd
+					return
+				}
+			case breakOpEq:
+				if valStr == bp.breakVal {
+					fmt.Println("hit breakpoint:", f, "==", valStr)
+					d.state = dbgStateNewCmd
+					return
+				}
+			case breakOpNeq:
+				if valStr != bp.breakVal {
+					fmt.Println("hit breakpoint:", f, "!=", bp.breakVal, "- now", valStr)
+					d.state = dbgStateNewCmd
+					return
+				}
+			default:
+				fmt.Println("unexpected bp op, something screwy's going on...")
+				d.state = dbgStateNewCmd
+				return
+			}
+		}
 		emu.Step()
 	}
 }
